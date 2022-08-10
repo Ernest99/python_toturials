30d366bc56196b4b4de5b8ffe140340be90a4c2b
vibrant_coder <74645353+Ernest99@users.noreply.github.com>
2022-08-10 23:36:17 +0000

    created replacement Program

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from math import *
+
+
 print('Hello world')
 name = 'Tim'
 age = 12
@@ -11,3 +14,28 @@ print(len(Name))
 print(name.replace('T','K'))
 print(type(Name))
 print(type(age))
+
+x = 'awesome'
+
+def myFunc():
+  global  x
+  x = 'great'
+  print('Python is ', x)
+
+myFunc()
+
+print('python is ' + x)
+
+print(sqrt(100))
+
+# name_detail = input('Enter your name: ')
+# print(name_detail)
+
+sentence = input('Enter your sentence: ')
+print(sentence)
+word_1 = input('Enter the word to replace: ')
+word_2 = input('Enter the word to replace it with: ')
+result = (sentence.replace(word_1,word_2))
+print(result)
+# for results in result:
+#     print(results)
