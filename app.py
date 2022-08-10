7e4a7f19c1d9585616ceb9bc2720062ceed74402
vibrant_coder <74645353+Ernest99@users.noreply.github.com>
2022-08-10 22:22:49 +0000

    basics of python

---
+++ app.py
@@ -0,0 +1,13 @@
+print('Hello world')
+name = 'Tim'
+age = 12
+# i used the python casting method for the 'age'[Casting is changing the data type of a specific data]
+print(" Hello " + name + ",\n I am " + str(age) + ' years old')
+#this is a python toturial for beginners
+Name = "Ernest"
+print(Name.lower().islower())
+print(len(Name))
+
+print(name.replace('T','K'))
+print(type(Name))
+print(type(age))
